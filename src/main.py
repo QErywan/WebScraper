23ef4087a03c114792cf8ffb415ea46652eb72f1
Qayyum <qerywan@gmail.com>
2024-01-22 13:57:23 +0000

    working one page

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 import pandas as pd
 import numpy as np
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import torch
 from sentence_transformers import SentenceTransformer
 from torch.nn.functional import cosine_similarity
@@ -17,32 +17,38 @@ import time
 
 
 # Bright Network
-
-base_url = "https://www.brightnetwork.co.uk/search/?content_types=jobs&career_path_sectors=Technology+%26+IT+Infrastructure&job_types=Internship&sort_by=recent"
+base_url = "https://www.brightnetwork.co.uk"
+url = "https://www.brightnetwork.co.uk/search/?offset=30&content_types=jobs&job_types=Internship&sort_by=recent"
 
 
 def parse_page(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'lxml')
     
 
     # Job Description
-    job_section = soup.find('div', class_='row g-0 result-row-with-calendar ps-3 p-lg-0 search-result-row')
-    job_description = job_section.find('div', class_='col-10 col-lg-8 description')
+    job_section = soup.find('section', class_='section__description')
+    job_description = job_section.get_text(strip=True) if job_section else None
+
 
     # Company Name
     company_name_tag = soup.find('a', href=re.compile(r"/graduate-employer-company/"))
     company_name = company_name_tag.get_text(strip=True) if company_name_tag else None
 
     # Deadline
-    deadline_tag = soup.find('div', class_='d-none d-lg-block date-container')
-    deadline = deadline_tag.get_text(strip=True) if deadline_tag else None
+    # comment = soup.find(text=lambda text: isinstance(text, Comment) and 'Deadline' in text)
+    # print(comment)
+    # deadline_tag = comment.find_next('div')
+    # deadline = deadline_tag.get_text(strip=True) if deadline_tag else None
+    
 
     # Role
-    role_tag = soup.find('h3', class_='h3 pt-2 pt-lg-3')
+    role_tag = soup.find('h1')
     role = role_tag.get_text(strip=True) if role_tag else None
 
-    return job_description, company_name, deadline, role
+    return job_description, company_name, role
+    # return company_name, role
+
 
 def get_next_page(soup):
     pagination_container = soup.find('div', class_='pagination-container')
@@ -54,13 +60,14 @@ def get_next_page(soup):
         
 def get_job_links(soup):
     job_links = []
-    print(soup.find_all('a', href=re.compile(r"/graduate-jobs/")))
-    for link in soup.find_all('a', href=re.compile(r"/graduate-jobs/")):
-
+    # print(soup.find_all('a', class_="read-more-arrow"))
+    for link in soup.find_all('a', class_="read-more-arrow"):
         job_links.append(f"{base_url}{link['href']}")
+    
+    return job_links
 
 
-current_url = base_url
+current_url = url
 original_url = base_url
 base_url = original_url.split("/search")[0]
 
@@ -74,25 +81,25 @@ page = 1
 
 while current_url:
     response = requests.get(current_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'lxml')
 
     job_links = get_job_links(soup)
 
     for job_link in job_links:
       #storing job descriptions
         job_texts.append(parse_page(job_link)[0])
-        roles.append(parse_page(job_link)[3])
-        deadlines.append(parse_page(job_link)[2])
+        roles.append(parse_page(job_link)[2])
+        # deadlines.append(parse_page(job_link)[2])
         companies.append(parse_page(job_link)[1])
         links.append(job_link)
       
         time.sleep(1)  # Add a delay between requests to avoid overloading the server
-
-    print(f"Scraped page {page}")
-
-    current_url = get_next_page(soup)
     
-    page += 1
-    time.sleep(1)  # Add a delay between requests to avoid overloading the server
+    current_url = get_next_page(soup)
 
-final_df = pd.DataFrame({'Job Description': job_texts, 'URL': links,'Company':companies,'Role':roles,'Deadline':deadlines})
+
+df = pd.DataFrame({'Company': companies, 'Role': roles, 'Link': links, 'Job Description': job_texts})
+df.to_excel("bright_network.xlsx")
+
+
+# print(parse_page("https://www.brightnetwork.co.uk/graduate-jobs/jacobs/project-support-summer-internship-bristol-2024?search_id=2b969117f6450052b7978ae982c05a65&search_position=6"))
