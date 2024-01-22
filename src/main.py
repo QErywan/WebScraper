e2ccb522aef9073f4d2f9f77fb7a629ab984db9d
Qayyum <qerywan@gmail.com>
2024-01-22 18:10:46 +0000

    Working bright network scraper

--- src/main.py
+++ src/main.py
@@ -1,24 +1,13 @@
 """ Job Board Aggregator """
-
-
-# LinkedIn
-
 import pandas as pd
-import numpy as np
 import requests
-from bs4 import BeautifulSoup, Comment
-import torch
-from sentence_transformers import SentenceTransformer
-from torch.nn.functional import cosine_similarity
-import matplotlib.pyplot as plt
-import seaborn as sns
-import re
+from bs4 import BeautifulSoup
 import time
 
 
-# Bright Network
+""" Bright Network """
 base_url = "https://www.brightnetwork.co.uk"
-url = "https://www.brightnetwork.co.uk/search/?offset=30&content_types=jobs&job_types=Internship&sort_by=recent"
+url = "https://www.brightnetwork.co.uk/search/?content_types=jobs&career_path_sectors=Technology+%26+IT+Infrastructure&job_types=Internship&sort_by=recent"
 
 
 def parse_page(url):
@@ -32,35 +21,33 @@ def parse_page(url):
 
 
     # Company Name
-    company_name_tag = soup.find('a', href=re.compile(r"/graduate-employer-company/"))
+    company_name_tag = soup.find('div', class_='field-related-company')
     company_name = company_name_tag.get_text(strip=True) if company_name_tag else None
 
     # Deadline
-    # comment = soup.find(text=lambda text: isinstance(text, Comment) and 'Deadline' in text)
-    # print(comment)
-    # deadline_tag = comment.find_next('div')
-    # deadline = deadline_tag.get_text(strip=True) if deadline_tag else None
+    deadline_tag = soup.find('div', class_='field-deadline')
+    deadline = deadline_tag.get_text(strip=True) if deadline_tag else None
+    if deadline != "Rolling deadline":
+        deadline = deadline.split("DEADLINE ")[1]
     
 
     # Role
     role_tag = soup.find('h1')
     role = role_tag.get_text(strip=True) if role_tag else None
 
-    return job_description, company_name, role
-    # return company_name, role
+    return (job_description, deadline, company_name, role)
 
 
 def get_next_page(soup):
     pagination_container = soup.find('div', class_='pagination-container')
     if pagination_container:
         next_page_tag = pagination_container.find_all('a')[-1]
-        if "Next" in next_page_tag.get_text(strip=True) and next_page_tag['href'] != 'None':
-            next_page_url = next_page_tag.find('a')['href']
-            return f"{base_url}{next_page_url['href']}"
+        if "Next" in next_page_tag.get_text(strip=True) and next_page_tag.has_attr('href'):
+            next_page_url = next_page_tag['href']
+            return f"{base_url}{next_page_url}"
         
 def get_job_links(soup):
     job_links = []
-    # print(soup.find_all('a', class_="read-more-arrow"))
     for link in soup.find_all('a', class_="read-more-arrow"):
         job_links.append(f"{base_url}{link['href']}")
     
@@ -75,7 +62,6 @@ base_url = original_url.split("/search")[0]
 job_texts = []
 companies=[]
 roles=[]
-links=[]
 deadlines=[]
 page = 1
 
@@ -87,19 +73,29 @@ while current_url:
 
     for job_link in job_links:
       #storing job descriptions
-        job_texts.append(parse_page(job_link)[0])
-        roles.append(parse_page(job_link)[2])
-        # deadlines.append(parse_page(job_link)[2])
-        companies.append(parse_page(job_link)[1])
-        links.append(job_link)
-      
+        page_parts = parse_page(job_link)
+        job_texts.append(page_parts[0])
+        roles.append((page_parts[3], job_link))
+        deadlines.append(page_parts[1])
+        companies.append(page_parts[2])
+
         time.sleep(1)  # Add a delay between requests to avoid overloading the server
     
+    print(f"Scraped page {page}")
+
     current_url = get_next_page(soup)
+    
+    page += 1
+    time.sleep(1)
 
+# Creates Excel hyperlinks
+def make_hyperlink(name, link):
+    return '=HYPERLINK("%s", "%s")' % (link, name)
 
-df = pd.DataFrame({'Company': companies, 'Role': roles, 'Link': links, 'Job Description': job_texts})
+# Create dataframe and export to Excel
+df = pd.DataFrame({'Company': companies, 'Role': roles, 'Deadline': deadlines})
+df['Role'] = df['Role'].apply(lambda x: make_hyperlink(x[0], x[1]))
 df.to_excel("bright_network.xlsx")
 
 
-# print(parse_page("https://www.brightnetwork.co.uk/graduate-jobs/jacobs/project-support-summer-internship-bristol-2024?search_id=2b969117f6450052b7978ae982c05a65&search_position=6"))
+""" LinkedIn """
